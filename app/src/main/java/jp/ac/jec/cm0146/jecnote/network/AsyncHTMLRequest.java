ef4f18bbb9804ba4f42014ec69272554725e4956
Shirai Yuto <21cm0146@jec.ac.jp>
2023-01-19 18:11:08 +0900

    ほぼ完成

--- app/src/main/java/jp/ac/jec/cm0146/jecnote/network/AsyncHTMLRequest.java
+++ app/src/main/java/jp/ac/jec/cm0146/jecnote/network/AsyncHTMLRequest.java
@@ -13,6 +13,9 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import jp.ac.jec.cm0146.jecnote.databinding.ActivityGuideViewBinding;
 import jp.ac.jec.cm0146.jecnote.utilities.JsonHelper;
@@ -64,13 +67,16 @@ public class AsyncHTMLRequest extends AsyncTask<Uri.Builder, Void, String> {
                 .replace("&gt;", ">")
                 .replace("\\n", "")
                 .replace("\\t", "")
-                .replace("<head>", "<head><meta name=\"viewport\" content=\"width=device-width\">");
+                .replace("<head>", "<head><meta name=\"viewport\" content=\"width=device-width\">")
+                .replace("<html>", "<html><style>span { color: red; }</style>");
 
-        if(!"".equals(keyword)) {
-            replace = replace
-                    .replace("<html>", "<html><style>span { color: red; }</style>")
-                    .replace(keyword, "<span><b>" + keyword + "</b></span>");
-        }
+        replace = protectHTML(replace);
+
+//        if(!"".equals(keyword)) {
+//            replace = replace
+//                    .replace("<html>", "<html><style>span { color: red; }</style>")
+//                    .replace(keyword, "<span><b>" + keyword + "</b></span>");
+//        }
 
 
         WebView wv = binding.webView;
@@ -80,6 +86,37 @@ public class AsyncHTMLRequest extends AsyncTask<Uri.Builder, Void, String> {
         settings.setBuiltInZoomControls(true);
         wv.loadDataWithBaseURL(null, replace, "text/html", "UTF8", null);
 
+    }
+
+    private String protectHTML(String html) {
+        List list = new ArrayList();
+        String pattern = "<[^>]*>"; // 正規表現
+        Pattern p = Pattern.compile(pattern); // 正規表現をコンパイルする？
+        Matcher m = p.matcher(html); // コンパイルされたやつをhtmlから探す
+
+        while (m.find()) {
+            list.add(m.group());
+        }
+
+        String[] images = new String[list.size()];
+        list.toArray(images);
+
+        html = html.replaceAll(pattern, "<>");
+
+        if(!"".equals(keyword)) {
+            html = html
+                    .replace(keyword, "<span><b>" + keyword + "</b></span>");
+        }
+
+//        System.out.println("replaceALL : " + html);
+        for(int i = 0; i < images.length; i++) {
+            html = html.replaceFirst("<>", images[i]);
+            System.out.println(images[i]);
+
+        }
+//        System.out.println("完成 ; " + html);
+
+        return html;
     }
 
     private String inputStreamToString(InputStream is) throws IOException {
